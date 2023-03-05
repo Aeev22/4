110452981c93bee3e40fd10dab6bf23c5e7f7d95
Ahmed <49731464+Aeev22@users.noreply.github.com>
2023-03-05 04:13:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 // Navbar
 var btn_icon = document.querySelector(".nav-icon-2");
 var btn = document.querySelector(".nav-right");
+var btn2 = document.querySelector(".nav-right ul");
 
 btn.onclick = function() {
-    btn.classList.toggle("d-block")
+    btn2.classList.toggle("d-block")
     btn_icon.classList.toggle('width-100')
 }
-
-var btn = document.querySelector(".nav-right ul");
-
-btn.onclick = function() {
-    btn.classList.toggle("d-block")
-    btn_icon.classList.toggle('width-100')
-}
-
